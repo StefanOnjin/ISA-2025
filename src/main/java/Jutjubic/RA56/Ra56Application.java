3ec6378dcf9169b63c2d2118941698bab51ae57f
Stefan Onjin <stefanonjin04@gmail.com>
2026-01-04 21:20:57 +0100

    Initial commit - Spring Boot project setup

---
+++ src/main/java/Jutjubic/RA56/Ra56Application.java
@@ -0,0 +1,13 @@
+package Jutjubic.RA56;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ra56Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ra56Application.class, args);
+	}
+
+}
